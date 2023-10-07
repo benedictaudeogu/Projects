f55730ccd220380481adf4d97e8714f233586241
Benedicta Udeogu <benedicta.u01@gmail.com>
2023-10-06 20:18:41 -0400

    Java Project

---
+++ PackageTracker/src/main/java/com/example/PackageTracker/PackageTrackageController.java
@@ -0,0 +1,4 @@
+package com.example.PackageTracker;
+
+public class PackageTrackageController {
+}
---
+++ PackageTracker/src/test/java/com/example/PackageTracker/PackageTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PackageTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackageTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
