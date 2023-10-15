e87d433058c76c1573fabfa807e37e3a9952bb67
Benedicta Udeogu <benedicta.u01@gmail.com>
2023-10-15 00:31:42 -0400

    Budget Project

--- Budget/src/main/java/org/example/CreateLinkToken.java
+++ Budget/src/main/java/org/example/CreateLinkToken.java
@@ -1,7 +1,6 @@
 package org.example;
 
 import java.io.IOException;
-import java.time.OffsetDateTime;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -11,19 +10,16 @@ import io.github.cdimascio.dotenv.Dotenv;
 
 import com.plaid.client.ApiClient;
 import com.plaid.client.model.CountryCode;
-// import com.plaid.client.model.ItemPublicTokenExchangeRequest;
-// import com.plaid.client.model.ItemPublicTokenExchangeResponse;
-// import com.plaid.client.model.LinkTokenAccountFilters;
 import com.plaid.client.model.LinkTokenCreateRequest;
 import com.plaid.client.model.LinkTokenCreateRequestUser;
 import com.plaid.client.model.LinkTokenCreateResponse;
 import com.plaid.client.model.Products;
 import com.plaid.client.request.PlaidApi;
 
-public class Main {
+public class CreateLinkToken {
     public static void main(String[] args) throws IOException {
         // System.out.println("Hello world!");
-        System.out.println("respose: " + createLinkToken());
+        System.out.println("respose body: " + createLinkToken());
     }
 
     public static LinkTokenCreateResponse createLinkToken() throws IOException {
@@ -35,37 +31,25 @@ public class Main {
         apiKeys.put("secret", dotenv.get("PLAID_SECRET")); 
         ApiClient apiClient = new ApiClient(apiKeys);
         apiClient.setPlaidAdapter(ApiClient.Sandbox);
-        System.out.println("apiClient: " + apiClient.toString());
-
+        // System.out.println("apiClient: " + apiClient.toString());
         PlaidApi plaidClient = apiClient.createService(PlaidApi.class);
-        System.out.println("plaidClient: " + plaidClient.toString());
+        // System.out.println("plaidClient: " + plaidClient.toString());
 
         // Create a User
         LinkTokenCreateRequestUser user = new LinkTokenCreateRequestUser().clientUserId("user-1");
-        user.setLegalName("benedicta");
-        // user.setPhoneNumber("1234567890");
-        // user.setPhoneNumberVerifiedTime(OffsetDateTime.parse("2023-10-13T15:30:00+01:00"));
-        // user.setEmailAddress("123@dmomain.com");
-        // user.setEmailAddressVerifiedTime(OffsetDateTime.parse("2023-10-13T15:30:00+01:00"));
-        // user.setSsn("123-45-6789");
-        // user.setDateOfBirth(OffsetDateTime.parse("2023-10-13T15:30:00+01:00"));
-        // System.out.println("user: " + user);
 
-        // // Create a link_token for the given user
+        // Create a link_token request for the given user
         LinkTokenCreateRequest request = new LinkTokenCreateRequest()
-                .clientId(dotenv.get("PLAID_CLIENT_ID"))
-                .secret(dotenv.get("PLAID_SECRET"))
                 .user(user)
                 .clientName("Personal Finance App")
                 .products(Arrays.asList(Products.TRANSACTIONS, Products.AUTH))
                 .countryCodes(List.of(CountryCode.US))
-                .language("en")
-                .redirectUri("http://localhost:3000")
-                .webhook("https://webhook.site/ce147318-d3ff-4d1c-8e93-11a141576d66");
+                .language("en");
         
         System.out.println("request: " + request);
         Response<LinkTokenCreateResponse> response = plaidClient.linkTokenCreate(request).execute();
-
+        System.out.println("response: " + response);
+        
         // Send the data to the client
         return response.body();
     }
