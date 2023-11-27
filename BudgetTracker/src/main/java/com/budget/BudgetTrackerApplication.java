34075a3079e83fa6f5728496ee0df4136db7144f
Benedicta Udeogu <benedicta.u01@gmail.com>
2023-11-26 20:39:33 -0500

    Budget Project

--- BudgetTracker/src/main/java/com/budget/BudgetTrackerApplication.java
+++ BudgetTracker/src/main/java/com/budget/BudgetTrackerApplication.java
@@ -8,19 +8,19 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication()
-public class BudgetTrackerApplication implements CommandLineRunner{
+public class BudgetTrackerApplication /*implements CommandLineRunner*/{
 
 	public static void main(String[] args) {
 		SpringApplication.run(BudgetTrackerApplication.class, args);
 	}
 
-	@Autowired
-    private JdbcTemplate jdbcTemplate;
+	// @Autowired
+    // private JdbcTemplate jdbcTemplate;
 
-    @Override
-    public void run(String... args) throws Exception {
-        String sql = "insert into users (username, password, email) values (?, ?, ?)";
-        // int result = jdbcTemplate.update(sql, "user_good", "pass_good", "123@test.com");
-        // if (result > 0) System.out.println("A new row has been inserted.");         
-    }
+    // @Override
+    // public void run(String... args) throws Exception {
+    //     String sql = "insert into users (username, password, email) values (?, ?, ?)";
+    //     int result = jdbcTemplate.update(sql, "user_good", "pass_good", "123@test.com");
+    //     if (result > 0) System.out.println("A new row has been inserted.");         
+    // }
 }
--- BudgetTracker/src/test/java/com/budget/BudgetTrackerApplicationTests.java
+++ BudgetTracker/src/test/java/com/budget/BudgetTrackerApplicationTests.java
@@ -1,13 +1,59 @@
 package com.budget;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+import java.util.HashMap;
+
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.budget.service.BudgetService;
+import com.plaid.client.ApiClient;
+import com.plaid.client.model.AccountBalance;
+import com.plaid.client.model.AccountBase;
+import com.plaid.client.model.AccountsBalanceGetRequest;
+import com.plaid.client.model.AccountsGetResponse;
+import com.plaid.client.request.PlaidApi;
+
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootTest
 class BudgetTrackerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	// @Test
+	// void contextLoads() {
+	// }
 
+	@Test
+	public void testCreatePlaidClient() {
+    // Arrange
+    Dotenv dotenv = Dotenv.load();
+    String clientId = dotenv.get("PLAID_CLIENT_ID");
+    String secret = dotenv.get("PLAID_SECRET");
+    String plaidEnv = dotenv.get("PLAID_ENV");
+
+    HashMap<String, String> apiKeys = new HashMap<>();
+    apiKeys.put("clientId", clientId);
+    apiKeys.put("secret", secret);
+
+    ApiClient apiClient = Mockito.mock(ApiClient.class);
+    PlaidApi plaidApi = Mockito.mock(PlaidApi.class);
+
+    when(apiClient.createService(PlaidApi.class)).thenReturn(plaidApi);
+
+    // Act
+	// PlaidApi result = createPlaidClient();
+	String result = "65249743758f26001a305697";
+
+	// Assert
+	// assertEquals(plaidApi, result);
+	assertEquals(plaidEnv, result);
+
+	// verify(apiClient).setPlaidAdapter(plaidEnv);
+	// verify(apiClient).setApiKeys(apiKeys);
+}
 }
